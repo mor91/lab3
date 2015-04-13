1ada5cc2c3e5cb85901b1dbad1fc7652bdef89d0
Mor <mor91@live.com>
2015-04-13 20:16:50 +0300

    saved 1bb

---
+++ 1b/linkedList.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void list_print(virus *virus_list){
+	virus *tempVirus=*virus_list;
+	while()
+}
+int main(int argc, char* argv[])
+{
+	typedef struct virus virus;
+	 
+	struct virus {
+		int length;
+		char *signature;
+		char *name;
+		virus *next;
+	};
+   	return(0);
+}
--- viruses.c
+++ viruses.c
@@ -1,39 +1,63 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+int power(int num1, int num2){
+    int i;
+    int j=1;
+    for(i=0; i<num2 ;i++){
+        j*=num1;
+    }
+    return j;
+}
+
 int convertToInt(char string[]){
 	int len=strlen(string);
 	int i;
 	int res=0;
-	for(i=0; i<len; i++)
-		res=res*10+(string[i]-'0');
+	for(i=0; i<len; i++){
+		res=res+(power(256,i)*string[i]);        
+        }
 	return res;
 
 }
-/*void printHex(char buffer[], int length){
+void printHex(char buffer[], int length){
+    
     int i;
+    for(i=0; i<length;i++)
+    	printf("%02X ", buffer[i]);
+    printf("\n");
+}
+void split(char buffer[], int length){
+    int i=0;
+    int j;
     int N;
-    int temp=0;
-    while(temp<length){
+    while(i<length){
 	    char size[4];
 	    memset(size,0,sizeof(size));
-	    for(i=0; i<4;i++)
-	    	size[i]=buffer[i];
+            int tempsize=i+4;
+	    for(i; i<tempsize;i++)
+	    	size[tempsize-4-i]=buffer[i];
 	    N=convertToInt(size);
-            printf("%d", N);
 	    char signature[N]; 
-	    for( i=4; i<N; i++)
-		signature[i-4]=buffer[i];
-	    char name[100];
-	    for(i=N+4; buffer[i]!='\'; i++)
-		name[i-N-4]=buffer[i];
+            memset(signature,0,sizeof(signature));
+            int tempN=N+i;
+	    for( j=i ; j<tempN; j++)
+		signature[j-i]=buffer[j];
+	    i=j;
+            char name[100]; 
+            memset(name,0,sizeof(name));
+	    for(j=i; buffer[j]!=0 ; j++)
+		name[j-i]=buffer[j];
+	    i=j+1;
 	    printf("Virus name: %s\n", name);
 	    printf("Virus size: %d\n", N);
-	    printf("signature: %02x \n", signature);
-	    temp+=i;
+            printf("signature:\n");
+	    printHex(signature, sizeof(signature));
+            printf("\n");
     }
 
-}*/
+}
 int main(int argc, char* argv[])
 {
    FILE *fp;
@@ -41,15 +65,16 @@ int main(int argc, char* argv[])
    memset(buffer,0,sizeof(buffer));
    fp = fopen("signatures", "r");
    fseek(fp, 0, SEEK_SET);
-   int length=fread(buffer, 1 ,sizeof(buffer), fp);
+   int length=fread(buffer,1, sizeof(buffer) , fp);
    int i;
-   for(i=0 ;i<sizeof(buffer) ;i++){
+   /*for(i=0 ;i<length ;i++){
    	printf("%c %d %02x\n", buffer[i], buffer[i], buffer[i]);
    }
    for(i=0 ;i<sizeof(buffer) ;i++){
    	printf("%c ", buffer[i]);
-   }
-   /*printHex(buffer, length);*/
+   }*/
+   split(buffer, length);
+   getchar();
    fclose(fp);
    return(0);
 }
